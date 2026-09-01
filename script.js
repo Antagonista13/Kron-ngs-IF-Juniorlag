d6461017c3ffc12c47239a6a79d8a88b07939ab2
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-01 10:19:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -101,5 +101,4 @@ document
   .querySelector('[data-page="calendarPage"]')
   .addEventListener("click", loadSportAdminCalendar);
 
-const sportAdminUrl =
-  "https://portalweb.sportadmin.se/webcal?id=0d53fe29-f39f-461d-bbd9-f376c64bc7f1";
+
