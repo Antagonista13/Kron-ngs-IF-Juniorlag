23d04ba1dc737b2f75af5d1ad87a1710c25afef0
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-01 09:42:25 +0200

    Add event listener to challenge button

---
+++ script.js
@@ -0,0 +1,9 @@
+const challengeButton = document.getElementById("challengeButton");
+
+challengeButton.addEventListener("click", () => {
+
+  challengeButton.textContent = "UTMANING KLAR! 🔥";
+
+  challengeButton.style.background = "#1f7a4f";
+
+});
