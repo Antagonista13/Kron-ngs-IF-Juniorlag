707df7b30a89a8d37ea46c8bd4ffea140d451b5f
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-02 19:47:40 +0200

    Add create-goal request test

---
+++ tests/goal-create.test.js
@@ -0,0 +1,18 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const goalCreate = require('../goal-create.js');
+
+test('builds a trimmed request for creating a new goal', () => {
+  assert.deepEqual(
+    goalCreate.buildGoalCreateRequest(
+      '  Bli modigare i mitt spel  ',
+      '  Jag vill våga ta fler initiativ.  ',
+      '  Jag tar fler initiativ under träning och match.  '
+    ),
+    {
+      p_title: 'Bli modigare i mitt spel',
+      p_description: 'Jag vill våga ta fler initiativ.',
+      p_success_description: 'Jag tar fler initiativ under träning och match.'
+    }
+  );
+});
