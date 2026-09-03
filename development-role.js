7dda4637af38b262ba927b100de7fb9a54b46e16
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-03 09:41:39 +0200

    feat: define player development card visibility by role

--- development-role.js
+++ development-role.js
@@ -2,10 +2,15 @@ function canEditSelfAssessment(role) {
   return role === "player";
 }
 
+function shouldShowPlayerDevelopmentCards(role) {
+  return role === "player";
+}
+
 if (typeof module !== "undefined" && module.exports) {
-  module.exports = { canEditSelfAssessment };
+  module.exports = { canEditSelfAssessment, shouldShowPlayerDevelopmentCards };
 }
 
 if (typeof window !== "undefined") {
   window.canEditSelfAssessment = canEditSelfAssessment;
+  window.shouldShowPlayerDevelopmentCards = shouldShowPlayerDevelopmentCards;
 }
