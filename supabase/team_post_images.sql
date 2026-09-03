3eae90af78e30da5ee687cef1952adc80559cc3c
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-03 14:10:57 +0200

    chore: remove unsupported SQL include helper

--- supabase/team_post_images.sql
+++
@@ -1,3 +0,0 @@
--- Kör denna i Supabase SQL Editor om projektet inte kör migrations automatiskt.
--- Den motsvarar migrationen 202609030001_team_post_images.sql.
-\i migrations/202609030001_team_post_images.sql
