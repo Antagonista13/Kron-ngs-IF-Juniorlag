1d4cefbaa231991836ad1678c0d08abc3d9be25c
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-07 18:11:44 +0200

    test: compact history mobile font

---
+++ tests/player-development-compact-regression-v49.test.js
@@ -0,0 +1,1 @@
+const test=require('node:test');const assert=require('node:assert/strict');const fs=require('node:fs');test('phone history labels use compact font',()=>{assert.match(fs.readFileSync('player-development-compact.css','utf8'),/font-size:11px/);});
