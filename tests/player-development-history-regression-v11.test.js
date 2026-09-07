f930dffb4ad82185dacd48f774f399cc48b7b316
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-07 18:05:31 +0200

    test: history default is deterministic

---
+++ tests/player-development-history-regression-v11.test.js
@@ -0,0 +1,1 @@
+const test=require('node:test');const assert=require('node:assert/strict');const fs=require('node:fs');test('history starts deterministically on goal history',()=>{assert.match(fs.readFileSync('player-development-layout.js','utf8'),/activeHistory='goal'/);});
