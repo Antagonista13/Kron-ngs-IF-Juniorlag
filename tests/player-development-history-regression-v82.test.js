5f0df1d5473460fd873024c056de1ee3d08007c2
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-07 18:16:37 +0200

    test: history state defaults before renderer

---
+++ tests/player-development-history-regression-v82.test.js
@@ -0,0 +1,1 @@
+const test=require('node:test');const assert=require('node:assert/strict');const fs=require('node:fs');test('history default state is declared before renderer',()=>{const js=fs.readFileSync('player-development-layout.js','utf8');assert.ok(js.indexOf("let activeHistory='goal'")<js.indexOf('function showHistory'));});
