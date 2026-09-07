a7c7765f3511926b129471c3f261a02fc2f6e6ed
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-07 18:14:29 +0200

    test: history active selection starts goal

---
+++ tests/player-development-history-regression-v68.test.js
@@ -0,0 +1,1 @@
+const test=require('node:test');const assert=require('node:assert/strict');const fs=require('node:fs');test('history starts with goal selected',()=>{assert.match(fs.readFileSync('player-development-layout.js','utf8'),/activeHistory='goal'/);});
