1157f52e00ce1fa01f7ae66bdb78bd07ce2cd344
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-07 18:12:03 +0200

    test: compact stylesheet scoped class

---
+++ tests/player-development-compact-regression-v51.test.js
@@ -0,0 +1,1 @@
+const test=require('node:test');const assert=require('node:assert/strict');const fs=require('node:fs');test('compact assessment rules require player compact class',()=>{assert.match(fs.readFileSync('player-development-compact.css','utf8'),/\.player-development-compact/);});
