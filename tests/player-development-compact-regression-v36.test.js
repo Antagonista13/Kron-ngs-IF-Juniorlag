159644dd0bdcb124c9a1dd54bc05ff4ed95ea9e6
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-07 18:09:50 +0200

    test: compact history pills have no border

---
+++ tests/player-development-compact-regression-v36.test.js
@@ -0,0 +1,1 @@
+const test=require('node:test');const assert=require('node:assert/strict');const fs=require('node:fs');test('history pills avoid bulky borders',()=>{assert.match(fs.readFileSync('player-development-compact.css','utf8'),/player-history-tabs button\{border:0/);});
