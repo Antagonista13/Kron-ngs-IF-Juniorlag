410e8340f68d727cea963c20a73ebe5bf9a8455f
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-07 18:08:32 +0200

    test: compact history selected weight

---
+++ tests/player-development-compact-regression-v27.test.js
@@ -0,0 +1,1 @@
+const test=require('node:test');const assert=require('node:assert/strict');const fs=require('node:fs');test('selected history control has stronger weight',()=>{assert.match(fs.readFileSync('player-development-compact.css','utf8'),/aria-selected="true"[^}]*font-weight:800/);});
