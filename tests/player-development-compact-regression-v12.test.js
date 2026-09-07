ea08c70f72d288e4b9d804c8efa6d9e1e74c1da9
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-07 18:05:47 +0200

    test: compact css mobile breakpoint

---
+++ tests/player-development-compact-regression-v12.test.js
@@ -0,0 +1,1 @@
+const test=require('node:test');const assert=require('node:assert/strict');const fs=require('node:fs');test('compact player styles include phone breakpoint',()=>{assert.match(fs.readFileSync('player-development-compact.css','utf8'),/@media\(max-width:560px\)/);});
