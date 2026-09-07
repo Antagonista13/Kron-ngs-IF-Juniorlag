f4c10a909d6880c88ee4d1b917ab355b2cfc3c5b
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-07 18:03:51 +0200

    test: compact stylesheet loaded last

---
+++ tests/player-development-compact-regression-v3.test.js
@@ -0,0 +1,1 @@
+const test=require('node:test');const assert=require('node:assert/strict');const fs=require('node:fs');test('compact stylesheet loads after base development styles',()=>{const html=fs.readFileSync('index.html','utf8');assert.ok(html.indexOf('player-development-compact.css')>html.indexOf('development-workflow.css'));});
