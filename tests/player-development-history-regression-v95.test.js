452a154c769092f42e4652579d9779c11c8d4223
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-07 18:18:37 +0200

    test: history selected key can be focus

---
+++ tests/player-development-history-regression-v95.test.js
@@ -0,0 +1,1 @@
+const test=require('node:test');const assert=require('node:assert/strict');const fs=require('node:fs');test('focus is a valid history panel key',()=>{assert.match(fs.readFileSync('player-development-layout.js','utf8'),/data-history-panel="focus"/);});
