a5327099cdea8901bf0322bf82850441ea11f7b4
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-07 18:15:48 +0200

    test: compact history controls inherit font

---
+++ tests/player-development-compact-regression-v76.test.js
@@ -0,0 +1,1 @@
+const test=require('node:test');const assert=require('node:assert/strict');const fs=require('node:fs');test('history controls inherit app font',()=>{assert.match(fs.readFileSync('player-development-compact.css','utf8'),/font:inherit/);});
