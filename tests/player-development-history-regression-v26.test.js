9c1b10e449862832a9fea8e10d666638d8418219
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-07 18:08:19 +0200

    test: history click uses show function

---
+++ tests/player-development-history-regression-v26.test.js
@@ -0,0 +1,1 @@
+const test=require('node:test');const assert=require('node:assert/strict');const fs=require('node:fs');test('history click delegates to showHistory',()=>{assert.match(fs.readFileSync('player-development-layout.js','utf8'),/addEventListener\('click',\(\)=>showHistory/);});
