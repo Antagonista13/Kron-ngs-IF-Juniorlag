1b1d301845097c509f0a3507b4dc3ec1309f4e0f
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-07 21:35:51 +0200

    test: require player development cache version 3

--- tests/player-development-layout.test.js
+++ tests/player-development-layout.test.js
@@ -4,7 +4,7 @@ const fs=require('node:fs');
 const html=fs.readFileSync('index.html','utf8');
 
 test('player Development has an explicit ordered journey contract',()=>{
-  assert.match(html,/player-development-layout\.js\?v=2/);
+  assert.match(html,/player-development-layout\.js\?v=3/);
   const layout=fs.readFileSync('player-development-layout.js','utf8');
   for(const id of ['playerMainGoalSlot','playerFocusSlot','playerAssessmentSlot','playerTrendSlot','playerHistorySlot']) assert.match(layout,new RegExp(id));
   assert.match(layout,/\[goalSlot,focusSlot,assessmentSlot,trendSlot,historySlot\]/);
