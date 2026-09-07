63ad1fc68727f578b0e222aa602e71b6eeda857c
Antagonista13 <h.bergqvist@hotmail.se>
2026-09-07 18:02:28 +0200

    test: updated layout asset loads

---
+++ tests/player-development-layout-loaded.test.js
@@ -0,0 +1,1 @@
+const test=require('node:test');const assert=require('node:assert/strict');const fs=require('node:fs');test('updated layout asset is loaded',()=>{assert.match(fs.readFileSync('index.html','utf8'),/player-development-layout\.js\?v=3/);});
